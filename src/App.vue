7285e8833ed90ac051b7604f4808c0f932beb713
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-05-20 17:56:01 +0200

    fatto

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@
     
     <HeaderComp @movieSearching="movieSearching"/>
 
-    <MainComp :movieSearched="movieSearched"/>
+    <MainComp :movieList="movieList"/>
 
   </div>
 </template>
 
 <script>
+import axios from "axios"
 import HeaderComp from "./components/HeaderComp.vue"
 import MainComp from "./components/MainComp.vue"
 
@@ -22,17 +23,38 @@ export default {
 
   data(){
     return{
+      apiUrl: "https://api.themoviedb.org/3/search/movie",
+
+      apiParams: {
+        api_key: "e39b9201864cae31abf65f1dcac8bcab",
+        language: "it_IT",
+        query: ""
+      },
+
+      movieList: [],
+
       movieSearched: ""
     }
   },
 
-  methods:{
+  methods: {
+    getApi() {
+      axios.get(this.apiUrl, {
+      params: this.apiParams
+      })
+      .then(res => {
+      this.movieList = res.data.results;
+      console.log(this.movieList);
+      });
+    },
+
     movieSearching(movieSearched){
-      this.movieSearched = movieSearched
-      console.log(movieSearched);
+      
+      this.apiParams.query = movieSearched
+      this.getApi()
+      console.log(this.apiParams.query);
     }
   }
-
 }
 </script>
 
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <CardComp 
-      v-for="(movie, index) in movieListFiltered" 
+      v-for="(movie, index) in movieList" 
       :key="`movie${index}`" 
       :movie="movie"
     />
@@ -9,59 +9,17 @@
 </template>
 
 <script>
-import axios from "axios"
 import CardComp from "./CardComp.vue";
-
 
 export default {
   name: "MainComp",
 
-  props:{
-    movieSearched: String
-  },
-
   components: { CardComp },
 
-  data() {
-    return {
-      apiUrl: "https://api.themoviedb.org/3/search/movie",
 
-      apiParams: {
-        api_key: "e39b9201864cae31abf65f1dcac8bcab",
-        language: "it-IT",
-        query: ""
-        },
-
-        movieList: []
-      }
-    },
-
-  mounted() {
-    this.getApiUrl();
+  props:{
+    movieList: Array
   },
-
-  methods: {
-    getApiUrl() {
-      axios.get(this.apiUrl, {
-      params: this.apiParams
-      })
-      .then(res => {
-      this.movieList = res.data.results;
-      console.log(this.movieList);
-      });
-    },
-
-    movieSearching(movieSearched){
-      this.query = movieSearched
-    }
-
-  },
-
-  computed:{
-    movieListFiltered(){
-      return this.movieList.filter(this.query)
-    }
-  }
     
 }
 </script>

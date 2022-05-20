f895b5b739274d62fa5c66dfda6dabbdbe0e29ae
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-05-20 12:52:50 +0200

    header emits

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -4,8 +4,8 @@
   <div class="container-fluid d-flex justify-content-between align-items-center">
     <h4>logo</h4>
     <div class="change">
-      <input type="text">
-      <select name="" id="" class="mx-2">
+      <input type="text" v-model.trim="movieSearched" @keyup.enter="$emit('movieSearching', movieSearched)">
+      <select v-model.trim="genreSelected" @change="$emit('genreSelecting', genreSelected)" name="search" id="" class="mx-2">
         <option value=""></option>
       </select>
     </div> 
@@ -16,7 +16,14 @@
 
 <script>
 export default {
-  name:"HeaderComp"
+  name:"HeaderComp",
+
+  data(){
+    return{
+      movieSearched: "",
+      genreSelected: ""
+    }
+  }
 }
 </script>
 

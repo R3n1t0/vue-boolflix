4bf3ebcc76bea30f3c458635e120b83b7d672465
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-05-20 12:05:15 +0200

    chiamata axios

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,35 @@
 <script>
 import HeaderComp from "./components/HeaderComp.vue"
 import MainComp from "./components/MainComp.vue"
+import axios from "axios"
+import { METHODS } from "http"
 
 export default {
   name: 'App',
+
   components: {
     HeaderComp,
     MainComp
+  },
+
+  data(){
+    return{
+      apiUrl: "https://developers.themoviedb.org/3",
+      movieList: []
+    };
+  },
+
+  mounted(){
+    this.getApiUrl();
+  },
+
+  methods:{
+    getApiUrl(){
+      axios.get(this.apiUrl)
+      .then(res => {
+        this.movieList = res.data
+      })
+    }
   }
 }
 </script>
